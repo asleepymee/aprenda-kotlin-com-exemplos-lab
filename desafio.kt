c62053e48755aef2f685b5d6814002ba3e9a89c3
Emilly <165350675+asleepymee@users.noreply.github.com>
2025-03-04 16:55:05 -0300

    Incluídas as funções de adicionar usuários e conteúdos pré-definidos no código. Além de mostrar no prompt os detalhes da formação e dos usuários.

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,47 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-data class Usuario
+data class Usuario(val name: String)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val name: String, var conteudos: MutableList<ConteudoEducacional> = mutableListOf(), val nivel: Nivel) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        //TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+    }
+    
+    fun addConteudo(conteudo: ConteudoEducacional){
+        conteudos.add(conteudo)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    //TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
+    //TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val formacao = Formacao("Analista de Dados", nivel = Nivel.BASICO)
+    
+	formacao.addConteudo(ConteudoEducacional(nome = "ANÁLISE COMBINATÓRIA"))
+    formacao.addConteudo(ConteudoEducacional(nome = "MATEMÁTICA E LÓGICA", 200))
+    
+    var usuario = Usuario("Bruno da Silva")
+    formacao.matricular(usuario)
+    usuario = Usuario("Alex Santos")
+    formacao.matricular(usuario)
+    
+    println("Grade curricular da formação de '${formacao.name}'\n(Nível de dificuldade: ${formacao.nivel}):")
+    println("================<>================")
+    formacao.conteudos.forEach{
+        conteudo -> println("Título: ${conteudo.nome} \nduração: ${conteudo.duracao}")
+        println("==================================")
+    }
+    println("Alunos inscritos:")
+    println("================<>================")
+    formacao.inscritos.forEach{
+        usuario -> println("Nome: ${usuario.name}")
+        println("==================================")
+    }
 }
